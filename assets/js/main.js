201e8c34342e376b11f6ed4928448a46b48e7eb8
Adyna Lima <adynalsl@gmail.com>
2023-05-26 22:45:45 -0300

    :construction:feat: iniciando js.

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#edit-input');
+const btnCancel = document.querySelector('#btn-cancel');
+
+
+todoForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const inputValue = todoInput.value
+
+    if (inputValue) {
+        console.log(inputValue)
+    }
+})
