434711e07ebcc4e566f23f4d813d1575567d4e69
Adyna Lima <adynalsl@gmail.com>
2023-05-28 23:41:24 -0300

    :construction:feat: finalizando js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,115 @@
 //Elementos 
-const todoForm = document.querySelector('#todo-form');
-const todoInput = document.querySelector('#todo-input');
-const todoList = document.querySelector('#todo-list');
-const editForm = document.querySelector('#edit-form');
-const editInput = document.querySelector('#edit-input');
-const btnCancel = document.querySelector('#btn-cancel');
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const btnCancel = document.querySelector("#btn-cancel");
+
+let oldInputValue; 
 
 //Funções
 const saveTodo = (text) => {
 
-    const todo = document.createElement("div")
-    todo.classList.add("todo")
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
 
-    const todoTitle = document.createElement("h3")
-    todoTitle.innerText = text
-    todo.appendChild(todoTitle)
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
 
-    const doneBtn = document.createElement("button")
-    doneBtn.classList.add("concluded")
-    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
-    todo.appendChild(doneBtn)
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("concluded");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
 
-    const editBtn = document.createElement("button")
-    editBtn.classList.add("edit")
-    editBtn.innerHTML = '<i class="fa-sharp fa-solid fa-pen"></i>'
-    todo.appendChild(editBtn)
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit");
+    editBtn.innerHTML = '<i class="fa-sharp fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
 
-    const removeBtn = document.createElement("button")
-    removeBtn.classList.add("remove")
-    removeBtn.innerHTML = '<i class="fa-solid fa-x"></i>'
-    todo.appendChild(removeBtn)
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove");
+    removeBtn.innerHTML = '<i class="fa-solid fa-x"></i>';
+    todo.appendChild(removeBtn);
 
-    todoList.appendChild(todo)
+    todoList.appendChild(todo);
 
     todoInput.value = "";
-}
+    todoInput.focus();
+};
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector("h3");
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    });
+};
 
 //Eventos
 todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = todoInput.value;
+
+    if (inputValue) {
+        saveTodo(inputValue);
+    }
+});
+
+document.addEventListener("click", (e) => {
+
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle; 
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
+
+    if (targetEl.classList.contains("concluded")) {
+        parentEl.classList.toggle("done");
+    }
+
+    if (targetEl.classList.contains("remove")) {
+        parentEl.remove();
+    }
+
+    if (targetEl.classList.contains("edit")) {
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
+});
+
+btnCancel.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
 
     e.preventDefault();
 
-    const inputValue = todoInput.value
+    const editInputValue = editInput.value;
 
-    if (inputValue) {
-        saveTodo(inputValue)
+    if(editInputValue) {
+        updateTodo(editInputValue)
     }
-})
+
+    toggleForms();
+});
